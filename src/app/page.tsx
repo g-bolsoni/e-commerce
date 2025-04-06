556151d93511f0417b64ede756b4c461ac9638e9
g-bolsoni <giovanebolsoni2@gmail.com>
2025-04-05 23:51:40 -0300

    feat: add image for mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <Image src="/images/d_home_banner.avif" priority={true} alt="banner home" width={1920} height={600} className="w-screen h-full" />
+      <picture>
+        <source media="(max-width: 1023px)" srcSet="/images/m_banner.avif" type="image/avif" />
+        <Image src="/images/d_home_banner.avif" priority={true} alt="banner home" width={1920} height={600} className="w-screen h-full" />
+      </picture>
       <section className="mx-3 md:container md:mx-auto">
         <ProductList />
         <Image src="/images/lancamento.avif" alt="banner destaques" width={1920} height={600} />
